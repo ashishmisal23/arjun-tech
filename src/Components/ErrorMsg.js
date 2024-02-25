34c8079048d651b02e98b27b7ae1433eb0332a84
ashishmisal23 <ashishmisal23@gmail.com>
2024-02-26 03:49:26 +0530

    Error Code Updated

--- src/Components/ErrorMsg.js
+++ src/Components/ErrorMsg.js
@@ -2,11 +2,21 @@ import React from 'react';
 import '../Style/error.css';
 
 
+
 const ErrorMsg = () => {
   return (
-    <div>
-      
-    </div>
+    <section className="primary-bg-color container" id="error-msg">
+      <div className="error">
+        <h1 className='large-heading'>Oops! An Error Occurred</h1>
+        <h1 className='third-large-heading'>The server returned a "404 Not Found".</h1>
+        <p className='md-para'>Something is broken. Please let us know what you were doing when this error occurred.
+          We will fix it as soon as possible. Sorry for any inconvenience caused.</p>
+      </div>
+
+
+    </section>
+
+
   )
 }
 
